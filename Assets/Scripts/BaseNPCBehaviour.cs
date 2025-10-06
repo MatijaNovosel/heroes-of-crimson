eb46c5444544dfdbcf0e2b36df6ccdc0015a2834
MatijaNovosel <matija.novosel@king-ict.eu>
2025-10-06 15:53:05 +0200

    Base npc behaviour

--- Assets/Scripts/BaseNPCBehaviour.cs
+++ Assets/Scripts/BaseNPCBehaviour.cs
@@ -7,6 +7,7 @@ public class BaseNPCBehaviour : MonoBehaviour
   // Public stats
   public float hp = 100;
   public float maxHp = 100;
+  public bool invincible = false;
 
   // Immunity
   protected float immuneTime = 1.0f;
@@ -27,12 +28,17 @@ public class BaseNPCBehaviour : MonoBehaviour
 
   protected virtual void Die()
   {
-    AudioSource.PlayClipAtPoint(deathSound, transform.position, 1.5f);
+    if (deathSound != null)
+    {
+      AudioSource.PlayClipAtPoint(deathSound, transform.position, 1.5f);
+    }
     Destroy(gameObject);
   }
 
   protected virtual void ReceiveDamage(float dmg)
   {
+    if (invincible) return;
+
     if (Time.time - lastImmune > immuneTime)
     {
       hp -= dmg;
