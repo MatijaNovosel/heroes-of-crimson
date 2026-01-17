6b4f46b77e980a7685e695b72741537f1ea8ae27
Matija Novosel <mnovosel5@gmail.com>
2026-01-17 21:46:11 +0100

    Talent tree wip

---
+++ Assets/Scripts/UI/TalentTree.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TalentTree : MonoBehaviour
+{
+    public bool TalentTreeOpen = true;
+    
+    private void HandleUIKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TalentTreeOpen = !TalentTreeOpen;
+            this.transform.localPosition = new Vector3(TalentTreeOpen ? 0 : 9999, 0, 0);
+        }
+    }
+
+    public void CloseTalentTree()
+    {
+        TalentTreeOpen = false;
+        this.transform.localPosition = new Vector3(9999, 0, 0);
+    }
+    
+    void Start()
+    {
+        //
+    }
+
+    void Update()
+    {
+        HandleUIKeys();
+    }
+}
