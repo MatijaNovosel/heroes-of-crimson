1a2d42b062b704a3ee34f4495dd5de3ad675891c
Matija Novosel <mnovosel5@gmail.com>
2026-03-13 23:05:31 +0100

    Fixes

--- Assets/Scripts/UI/DeathOverlay/DeathOverlay.cs
+++ Assets/Scripts/UI/DeathOverlay/DeathOverlay.cs
@@ -19,6 +19,8 @@ public class DeathOverlay : MonoBehaviour
     
     public void BackToMainMenu()
     {
+        Time.timeScale = 1f;
+        transform.localPosition = Vector3.positiveInfinity;
         SceneManager.LoadScene((int)Constants.Screens.MainMenu);
     }
 }
